a00be2a0958c7dc223eec65abcab888fb5e3d920
drpenknife <drpenknife@none.not>
2019-07-03 17:47:02 +0200

    fixes

--- rf2.js
+++ rf2.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 
 var f='fw_train_3min_uniq.csv'
 //f = 'tennis.csv' 
-//f = 'iris.csv' 
+f = 'iris.csv' 
 var dropheader = true
 
 var samprate = 0.9
@@ -72,7 +72,7 @@ function entropy(prob){
     return {val:-sum, p:probmap};
 }
 
-function groupby(x, c){
+function get_strats(x, c){
     let g = {}
     for(let i in x){
         let l = x[i][c]
@@ -125,45 +125,33 @@ function stsample(x,s,p){
 // Random Tree
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-function left_or_right(x, cid, val, region_size){
+function left_or_right(x, cid, val){
     if(colstats[cid].type == 'nominal'){
-        if(x[cid] == val)return 0
-		return 1
+        if(x[cid] == val)return 0; else return 1;
     }else{
-		//var r = Math.abs(colstats[cid].range.max-colstats[cid].range.min)
-		//if( Math.abs(x[cid]-val) <= region_size*r) return 0
-		//else return 1;
-		if(x[cid] < val) return 0;
-		else return 1;
+		if(x[cid] < val) return 0; else return 1;
     }
 }
 
-function split_col(x, colid, val, e0, rs){
+function split_col(x, colid, val, e0){
     var mp = [[],[]]
     var stat = [{},{}]
     var ln = [0,0]
     var L = x.length
-	let region_size = Math.random()*rs
     for(var i in x){
         var l = x[i][classid] 
-        var hit=left_or_right(x[i], colid, val, region_size)
+        var hit=left_or_right(x[i], colid, val)
         ln[hit]++;
         stat[hit][l] = stat[hit][l]?stat[hit][l]+1:1
         mp[hit].push(x[i]) 
     }
     var res={data:x, split:mp, stats:stat, 
-			ln:ln, scol:colid, val:val, rs:region_size}
+			ln:ln, scol:colid, val:val}
     if(ln[0] && ln[1]){
         res.ent = [entropy(stat[0]), entropy(stat[1])]
         res.gain = e0 - res.ent[0]*ln[0]/L - res.ent[1]*ln[1]/L
         return res
     }else return null
-}
-
-function split(node, col, sampleid){
-    var value = node.data[sampleid][col]
-    var r = split_col(node.data, col, value, node.entropy, node.rs)
-    return r
 }
 
 function bestsplit(node, maxdepth){
@@ -172,32 +160,32 @@ function bestsplit(node, maxdepth){
     for(var i=0;i<selections;i++){
 	   var col = randint(0, node.data[0].length-1)
 	   var sampleid = randint(0, node.data.length)
-	   var r = split(node, col, sampleid)
+	   var r = split_col(node.data, col, node.data[sampleid][col], node.entropy) 
 	   if(!best || r && (r.gain > best.gain)){
 		   best = r
 	   }
     }
     if(!best) return null;
-    var slices = {}
+    var slices = []
 
-    slices.L = {data:best.split[0], depth:node.depth+1, entropy:best.ent[0].val,
-                  id: 2*node.id+1, stats:best.ent[0].p, rs: best.rs}
-    slices.R = {data:best.split[1], depth:node.depth+1, entropy:best.ent[1].val, 
-                  id: 2*node.id+2, stats:best.ent[1].p, rs: best.rs}
-    node.split = {prop:best.scol, val:best.val, rs: best.rs}
+	for(let id=0; id<2; id++){
+		slices.push({data:best.split[id], depth:node.depth+1, entropy:best.ent[id].val,
+                  id: 2*node.id+(1+id), stats:best.ent[id].p})
+	}			  
+    node.split = {prop:best.scol, val:best.val}
     return slices;
 }
 
 function buildtree(data, entropy0, maxdepth){
     var tree = {}
-    var q = [{data:data, depth:0, entropy: entropy0.val, id:0, rs:0.1}]
+    var q = [{data:data, depth:0, entropy: entropy0.val, id:0}]
     while(q.length){
         var prnt = q.pop()
         var slices = bestsplit(prnt, maxdepth)
         tree[prnt.id] = prnt;
         if(slices){
-            if(slices.L)q.push(slices.L)
-            if(slices.R)q.push(slices.R)
+			for(let id=0; id<2; id++)
+				if(slices[id])q.push(slices[id])
         }else{
             prnt['isleaf']=true
         }
@@ -210,18 +198,14 @@ function buildmodel(indata, strats){
    console.log('training...');
    let trees =[]
    for(let i = 0; i < tressnum; i++){
-      //console.log('sampling data, rate = '+samprate)
       data = stsample(indata, strats, samprate).a
       ctr = hist(data, classid)
       entropy0 = entropy(ctr);
       console.log('training tree'+i)
       var t = buildtree(data, entropy0, maximumdepth)
       trees.push(t)
-      //evaluatemodel([t], fdata)
    }
-
    console.log('-=-=-=-=-=-=-=-=')
-
    return trees;
 }
 
@@ -252,7 +236,6 @@ function classify(tree, x){
        let u = single_tree_cls(tree[i], x);
        r[u]=r[u]?r[u]+1:1
     }
-   // console.log(r)
     return argmax(r)
 }
 
@@ -322,12 +305,12 @@ fs.readFile(f,'utf8', function(e,c){
    fdata = c.split('\n')
    for(var i=0;i<fdata.length;i++){
        fdata[i] = fdata[i].replace('\r','').replace('\n','').split(',');
-       fdata[i].pop()
+       //fdata[i].pop()
    }
    if(dropheader)header = fdata.splice(0,1)[0];
    classid = fdata[0].length - 1;
    
-   data_strats = groupby(fdata, classid)
+   data_strats = get_strats(fdata, classid)
   
    console.log("-=- Colstats =-=-=-=-")
 
@@ -336,8 +319,7 @@ fs.readFile(f,'utf8', function(e,c){
    console.log("-=-=-=-=-=-")
    
    let parts = stsample(fdata, data_strats, 0.8)
-   
-   let trainstrats = groupby(parts.a, classid)
+   let trainstrats = get_strats(parts.a, classid)
 
    let model = buildmodel(parts.a, trainstrats)
    evaluatemodel(model, parts.a)
