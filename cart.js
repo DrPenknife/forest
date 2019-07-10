f367946c498b87429c175ec1f8e82994aeafd7cf
drpenknife <drpenknife@none.not>
2019-07-10 23:35:33 +0200

    co ja robie?@ 1

--- cart.js
+++ cart.js
@@ -90,28 +90,93 @@ function calcstats(data){
 // Cart
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-function split(t){
-   let col = randint(0,classid-1)
-   let pid = randint(0,t.length-1)
-   let piv = t[pid][col]
 
-   t.sort(function(a,b) {
-       return a[col] - b[col]
-   })
-   
-   let spv=[]
+function impurity(t,sum){
+	let im = 1.0
+	for(let k in t){
+		let p = t[k]/=sum
+		im -= p*p
+	}
+	return im
+}
 
-   for(let i =1;i< t.length; i++){
-      if(t[i-1][classid]!=t[i][classid]){
-        spv.push((t[i-1][col]+t[i][col])/2)
-      }
+function gain(l,r){
+	let lm = {}
+	let ln = {}
+	let g = {}
+	
+	//console.log(l,r)
+	for(let i = 0; i < l.length; i++){
+		!lm[l[i][classid]]?lm[l[i][classid]]=1:lm[l[i][classid]]++
+		!g[l[i][classid]]?g[l[i][classid]]=1:g[l[i][classid]]++
+	}
+	
+	for(let i = 0; i < r.length; i++){
+		!ln[r[i][classid]]?ln[r[i][classid]]=1:ln[r[i][classid]]++
+		!g[r[i][classid]]?g[r[i][classid]]=1:g[r[i][classid]]++
+	}
+	let s = l.length + r.length
+	
+	return impurity(g,s)-((impurity(lm, l.length)*l.length)/s + (impurity(ln, r.length)*r.length)/s)
+}
+
+function split(t){	
+   let bestcol = null; //randint(0,classid-1)
+   let bestgain = -100
+   let bestsplit = 0
+   let maxlab = t[0][classid]
+   //console.log(t)	   
+   for(let col = 0; col < classid; col++){
+	   console.log("col=",col)
+	   let lab = {}
+	   let maxlabctr = -1
+	   
+	   t.sort(function(a,b) {
+		   return a[col] - b[col]
+	   })
+	   
+	   let spv=[]
+
+	   for(let i =1;i< t.length; i++){
+		  lab[t[i][classid]]?lab[t[i][classid]]++:lab[t[i][classid]]=1
+		  if(lab[t[i][classid]]>maxlabctr){
+			  maxlabctr=lab[t[i][classid]]
+			  maxlab = t[i][classid]
+		  }
+		  if(t[i-1][classid]!=t[i][classid]){
+			spv.push((t[i-1][col]+t[i][col])/2)
+		  }
+	   }
+	   
+	   //console.log(spv)
+	   
+	   
+	   for(let i = 0; i<spv.length; i++){
+			let ts = {L:[],R:[]}
+			for(let j=0; j < t.length; j++){
+				t[j][col] < spv[i]?ts.L.push(t[j]):ts.R.push(t[j])
+			}
+			
+			let g = gain(ts.L,ts.R)
+			if(g>bestgain)
+			{
+				bestgain = g
+				bestsplit = spv[i]
+				bestcol = col
+			}
+			
+	   }
    }
+   console.log("best",bestcol, bestsplit)
    
-   console.log(spv.length, t.length,spv)
-   let r = {L:[], R:[], col:col, p:piv}
+   if(bestgain < 0) return {split:null, lab:maxlab}
+   piv = bestsplit
+   //console.log(spv.length, t.length,spv)
+   let r = {split:{L:[], R:[], col:bestcol, p:piv}, lab:maxlab}
    for(let i=0; i < t.length; i++){
-      t[i][col] < piv?r.L.push(t[i]):r.R.push(t[i])
+      t[i][bestcol] < piv?r.split.L.push(t[i]):r.split.R.push(t[i])
    }
+   r.x=[r.split.R.length, r.split.L.length]
    return r;
 }
 
@@ -120,16 +185,19 @@ function buildmodel(tab){
    var q = [{d:tab, id:0, depth:0}]
    while(q.length){
        var n = q.pop()
-       if(n.d.length > 2 && n.depth < 12){
-           let s = split(n.d)
-           tr[n.id]={col:s.col, p:s.p}
-           q.push({d:s.L,id:n.id*2+1, 
-                   depth:n.depth+1})           
-           q.push({d:s.R,id:n.id*2+2, 
-		    	   depth:n.depth+1})
-       }else{
-           tr[n.id]={lab:1}
-   	}
+	   console.log(n.d.length)
+	   let s = split(n.d)
+	   if(!s.split){
+		   console.log("------",n.d, s.lab)
+		   continue;
+	   }else{
+		   //console.log(s.x)
+	   }
+	   tr[n.id]={col:s.col, p:s.p}
+	   if(s.split.L.length)q.push({d:s.split.L,id:n.id*2+1, 
+			   depth:n.depth+1})           
+	   if(s.split.R.length)q.push({d:s.split.R,id:n.id*2+2, 
+			   depth:n.depth+1})
        
    }
 
